42622a3411d1fcd4b463ef1936e11ddc2f729a37
tim40 <tim4054@gmail.com>
2024-07-30 21:11:04 +0300

    Initial commit

---
+++ src/main/java/me/nugumanov/shopmarket/ShopmarketApplication.java
@@ -0,0 +1,13 @@
+package me.nugumanov.shopmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmarketApplication.class, args);
+	}
+
+}
